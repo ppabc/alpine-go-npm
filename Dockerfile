5db8e200b685df6d514a29bc332aaaafb74c3421
aqzt <5553557@qq.com>
2018-12-02 08:57:24 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,9 @@ FROM golang:1.10-alpine3.8
 RUN apk add nodejs nodejs-dev npm git && rm -f /var/cache/apk/*
 
 COPY golang123 /go/golang123 
-COPY startup.sh /startup.sh 
+COPY install.sh /install.sh 
+COPY startup.sh /startup.sh
 
-RUN chmod +x /startup.sh && sh /startup.sh
+RUN chmod +x /startup.sh && chmod +x install.sh && sh /install.sh
+
+CMD ["/startup.sh"]
--- startup.sh
+++ startup.sh
@@ -1,11 +1,5 @@
-mkdir -p /go/src/github.com/shen100/
-mv /go/golang123 /go/src/github.com/shen100/golang123
 
 cd /go/src/github.com/shen100/golang123/website
-npm install
-##  86 npm audit fix
-##  npm audit fix --force
-##
-go get -u github.com/golang/dep/cmd/dep
+npm run dev > /data/dev.log &
 cd /go/src/github.com/shen100/golang123
-dep ensure
+go run main.go > /data/go.log &
