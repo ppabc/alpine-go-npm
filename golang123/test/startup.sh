06636cab41dbc08160c89f467c97466920b84a1b
ppabc <ppabc@qq.com>
2018-12-02 13:53:10 +0800

    update

--- golang123/test/startup.sh
+++ golang123/test/startup.sh
@@ -1,7 +1,7 @@
 cp /go/src/github.com/shen100/golang123/test/index.js /go/src/github.com/shen100/golang123/website/config/
-cp /go/src/github.com/shen100/golang123/test/config.json /go/src/github.com/shen100/golang123/
+cp /go/src/github.com/shen100/golang123/test/config1.json /go/src/github.com/shen100/golang123/config.json
 
 cd /go/src/github.com/shen100/golang123/website
-npm run dev > /data/dev.log &
+npm run dev > /tmp/dev.log &
 cd /go/src/github.com/shen100/golang123
-go run main.go > /data/go.log &
+go run main.go > /tmp/go.log &
